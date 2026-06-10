a28443512a1baa0a333034e4ec386d5fad3ca715
silverj1207 <silverj1207@gmail.com>
2026-06-10 21:57:01 +0900

    feat: localize playlist names and add new tracks
    Translate existing playlist names to Korean and expand the library with additional songs.

--- src/data.ts
+++ src/data.ts
@@ -3,7 +3,7 @@ import { Playlist } from './types';
 export const MOCK_PLAYLISTS: Playlist[] = [
   {
     id: 'folder-1',
-    name: 'Sector 7',
+    name: '섹터 7',
     songs: [
       { 
         id: 's1', 
@@ -23,7 +23,7 @@ export const MOCK_PLAYLISTS: Playlist[] = [
   },
   {
     id: 'folder-2',
-    name: 'Abel of the Black Season',
+    name: '검은 계절의 아벨',
     songs: [
       { 
         id: 's4', 
@@ -36,7 +36,7 @@ export const MOCK_PLAYLISTS: Playlist[] = [
   },
   {
     id: 'folder-3',
-    name: 'The Noxvale Brothers',
+    name: '녹스베일 형제',
     songs: [
       { 
         id: 's7', 
@@ -49,7 +49,7 @@ export const MOCK_PLAYLISTS: Playlist[] = [
   },
   {
     id: 'folder-4',
-    name: 'Elliot Valois',
+    name: '엘리엇 발루아',
     songs: [
       { 
         id: 's8', 
@@ -57,6 +57,71 @@ export const MOCK_PLAYLISTS: Playlist[] = [
         artist: '', 
         duration: '',
         url: 'https://www.dropbox.com/scl/fi/t47zfjln21sziquqqwesf/Noctis-in-the-Labyrinth.mp3?rlkey=wusz0lr9oflaci2whrx6z1drk&st=058d0x57&raw=1'
+      },
+    ],
+  },
+  {
+    id: 'folder-5',
+    name: '인큐버스 루루',
+    songs: [
+      { 
+        id: 's9', 
+        title: '', 
+        artist: '', 
+        duration: '',
+        url: 'https://www.dropbox.com/scl/fi/ecjlwp7b8wei1u9lf5akc/lulu.wav?rlkey=2ndadfxwbli2hglfkqox1gb16&st=ubn3npbv&raw=1'
+      },
+    ],
+  },
+  {
+    id: 'folder-6',
+    name: '둘 사이의 여름',
+    songs: [
+      { 
+        id: 's10', 
+        title: '', 
+        artist: '', 
+        duration: '',
+        url: 'https://www.dropbox.com/scl/fi/itiun3sr1jaijp6oxashm/Summer-of-Kamisaki.wav?rlkey=iqlus5rqqdppmip5vty2eknkb&st=ekjlv2nr&raw=1'
+      },
+    ],
+  },
+  {
+    id: 'folder-7',
+    name: '채무 상환 보고서',
+    songs: [
+      { 
+        id: 's11', 
+        title: '', 
+        artist: '', 
+        duration: '',
+        url: 'https://www.dropbox.com/scl/fi/u415wzin2xgqb2b94lq66/.mp3?rlkey=wmv2n0d00ov4cbb7tvevb799p&st=90d5lo0s&raw=1'
+      },
+    ],
+  },
+  {
+    id: 'folder-8',
+    name: '캘리스터 쌍둥이',
+    songs: [
+      { 
+        id: 's12', 
+        title: '', 
+        artist: '', 
+        duration: '',
+        url: 'https://www.dropbox.com/scl/fi/1k9kl0h9hf0zj5drudfbc/The-Callister-Twins.wav?rlkey=uukp3hohuikaf11bfzreyh2n6&st=uwpb6ylv&raw=1'
+      },
+    ],
+  },
+  {
+    id: 'folder-9',
+    name: '차이나타운',
+    songs: [
+      { 
+        id: 's13', 
+        title: '', 
+        artist: '', 
+        duration: '',
+        url: 'https://www.dropbox.com/scl/fi/5pc1hzn32i70uqyjiw5ty/chinatown_3.mp3?rlkey=psh434nfztmpszknp3df13dfx&st=v2z8xd5w&raw=1'
       },
     ],
   },
